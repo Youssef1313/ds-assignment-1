a15beed65efc75bffeb187fac9f9c88f4e63862b
mohamed zayton <mohamed-zayton>
2021-05-11 17:40:42 +0200

    ss_review

--- src/sorting/selection_sort.py
+++ src/sorting/selection_sort.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 
 class SelectionSort:
@@ -7,10 +6,7 @@ class SelectionSort:
         self.arr = arr
 
     def sort(self):
-        start: float = time.process_time()
         self.__sort_alg()
-        end: float = time.process_time()
-        print(end-start)
         return self.arr
 
     def __sort_alg(self):
@@ -23,8 +19,8 @@ class SelectionSort:
             self.arr[i], self.arr[min_index] = self.arr[min_index], self.arr[i]
 
 
-arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
-# arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
+# arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
+arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
 ss = SelectionSort(arr)
 print(ss.sort())
 
