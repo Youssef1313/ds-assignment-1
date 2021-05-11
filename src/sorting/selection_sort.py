c0450792ea644fbc114bdbc7df49836019f4e70a
mohamed zayton <mohamed-zayton>
2021-05-11 22:53:06 +0200

    static_SS

--- src/sorting/selection_sort.py
+++ src/sorting/selection_sort.py
@@ -2,24 +2,19 @@ import random
 
 
 class SelectionSort:
-    def __init__(self, arr):
-        self.arr = arr
 
-    def sort(self):
-        self.__sort_alg()
-        return self.arr
-
-    def __sort_alg(self):
-        arr_len = len(self.arr)
+    @staticmethod
+    def sort(arr):
+        arr_len = len(arr)
         for i in range(arr_len):
             min_index = i
             for j in range(i, arr_len):
-                if self.arr[min_index] > self.arr[j]:
+                if arr[min_index] > arr[j]:
                     min_index = j
-            self.arr[i], self.arr[min_index] = self.arr[min_index], self.arr[i]
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        return arr
 
 
 arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
-# arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
-ss = SelectionSort(arr)
-print(ss.sort())
+arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
+print(SelectionSort.sort(arr))
