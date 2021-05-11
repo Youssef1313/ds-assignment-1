ff8aaf58b547dd13630f053798f4b98b8b289560
Youssef1313 <youssefvictor00@gmail.com>
2021-05-11 13:23:48 +0200

    Add initial tests and make sure they run on GH Action

---
+++ tests/sorting/test_sorting.py
@@ -0,0 +1,25 @@
+# TODO: Remove noqa in all tests.
+
+def test_empty_list():
+    input = []  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_single_element():
+    input = [5]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_two_elements_already_sorted():
+    input = [1, 3]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_two_elements_unsorted():
+    input = [3, 1]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_sorting1():
+    input = [5, 3, 1, 9, 8, 4, 3, 2, 0, 9, 16, 15, 17]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
