066bc652d140ca2020a63adb857830f0e68063bc
Youssef Victor <youssefvictor00@gmail.com>
2021-05-11 05:29:15 +0200

    Apply suggestions from code review
    Co-authored-by: MOHAND99 <53486163+MOHAND99@users.noreply.github.com>

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -1,13 +1,13 @@
 def bubble_sort(unsorted):
     length = len(unsorted)
     for i in range(length):
-        madeAnySwaps = False
+        made_swaps = False
         for j in range(0, length - i - 1):
             if unsorted[j] > unsorted[j + 1]:
                 unsorted[j], unsorted[j + 1] = unsorted[j + 1], unsorted[j]
-                madeAnySwaps = True
+                made_swaps = True
 
-        if not madeAnySwaps:
+        if not made_swaps:
             break
 
 
