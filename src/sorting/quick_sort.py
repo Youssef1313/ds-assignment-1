266ba2788c666628a5246803aa7a047b5874709e
mohamed zayton <mohamed-zayton>
2021-05-11 17:38:00 +0200

    qs_review

--- src/sorting/quick_sort.py
+++ src/sorting/quick_sort.py
@@ -2,7 +2,7 @@ import random
 from abc import abstractmethod
 
 
-class QuickSort:
+class QuickSort():
 
     def sort(self):
         self.__quick_sort(0, len(self.arr))
@@ -18,7 +18,7 @@ class QuickSort:
     def __partition(self, first, last):
         i: int = first - 1
         j: int = first
-        piv: int = self.__get_piviot(first, last)
+        piv: int = self._get_piviot(first, last)
         while i < (last) and j < (last):
             if self.arr[j] < piv:
                 i += 1
@@ -28,7 +28,7 @@ class QuickSort:
         return i+1
 
     @abstractmethod
-    def __get_piviot(self, first, last):
+    def _get_piviot(self, first, last):
         pass
 
 
@@ -36,7 +36,7 @@ class QuickSortLP(QuickSort):
     def __init__(self, arr):
         self.arr = arr
 
-    def __get_piviot(self, first, last):
+    def _get_piviot(self, first, last):
         return self.arr[last]
 
 
@@ -44,15 +44,28 @@ class QuickSortRP(QuickSort):
     def __init__(self, arr):
         self.arr = arr
 
-    def __get_piviot(self, first, last):
+    def _get_piviot(self, first, last):
         rand: int = random.randint(first, last)
         return self.arr[rand]
 
 
 # arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
 arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
-
 q1 = QuickSortLP(arr)
 q2 = QuickSortRP(arr)
 print(q1.sort())
 print(q2.sort())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
