9c23417f4f7eea2ddb6e7f83390effe82a4a2ece
Youssef1313 <youssefvictor00@gmail.com>
2021-05-11 14:00:02 +0200

    Add more test cases

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
 # TODO: Remove noqa in all tests.
 
+def test_none():
+    input = None  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_not_a_list():
+    input = "Hello world"  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_list_containing_none():
+    input = [1, None, 5]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
 def test_empty_list():
     input = []  # noqa: F841
     assert True  # TODO: Test against the 6 algorithms.
@@ -17,6 +32,11 @@ def test_two_elements_already_sorted():
 
 def test_two_elements_unsorted():
     input = [3, 1]  # noqa: F841
+    assert True  # TODO: Test against the 6 algorithms.
+
+
+def test_integers_and_floats():
+    input = [5, 3.1, 3, 5.5, 1, 2.1]  # noqa: F841
     assert True  # TODO: Test against the 6 algorithms.
 
 
