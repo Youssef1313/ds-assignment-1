b052d8e12a11b9cb8f3f5915e9994d387a1c0ede
mohamed-zayton <47240722+mohamed-zayton@users.noreply.github.com>
2021-05-12 15:24:47 +0200

    Modify selection sort to a free-function in module instead of static stateless class (#23)
    * Modify selection sort to a free-function in module instead of static stateless class
    
    * Apply suggestions from code review
    
    Co-authored-by: mohamed zayton <mohamed-zayton>
    Co-authored-by: Youssef Victor <youssefvictor00@gmail.com>

--- src/sorting/selection_sort.py
+++ src/sorting/selection_sort.py
@@ -1,20 +1,13 @@
-import random
+def selection_sort(unsorted):
+    arr_len = len(unsorted)
+    for i in range(arr_len):
+        min_index = i
+        for j in range(i, arr_len):
+            if unsorted[min_index] > unsorted[j]:
+                min_index = j
+        unsorted[i], unsorted[min_index] = unsorted[min_index], unsorted[i]
 
 
-class SelectionSort:
-
-    @staticmethod
-    def sort(arr):
-        arr_len = len(arr)
-        for i in range(arr_len):
-            min_index = i
-            for j in range(i, arr_len):
-                if arr[min_index] > arr[j]:
-                    min_index = j
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-        return arr
-
-
-arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
 arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
-print(SelectionSort.sort(arr))
+selection_sort(arr)
+print(arr)
