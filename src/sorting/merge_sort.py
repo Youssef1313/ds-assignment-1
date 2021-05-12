da7bddae55987584f9ef6cacb8ceffde99c41128
MOHAND99 <muhanned122000@yahoo.com>
2021-05-12 16:32:00 +0200

    sort in-place

--- src/sorting/merge_sort.py
+++ src/sorting/merge_sort.py
@@ -3,7 +3,9 @@ def merge_sort(unsorted):
         return unsorted
 
     unsorted_half1, unsorted_half2 = divide(unsorted)
-    return merge(merge_sort(unsorted_half1), merge_sort(unsorted_half2))
+    return merge(merge_sort(unsorted_half1),
+                 merge_sort(unsorted_half2),
+                 unsorted)
 
 
 def divide(unsorted):
@@ -11,23 +13,24 @@ def divide(unsorted):
     return unsorted[:half_i], unsorted[half_i:]
 
 
-def merge(sorted_half1, sorted_half2):
+def merge(sorted_half1, sorted_half2, unsorted):
     i, j, k = 0, 0, 0
     half1_length, half2_length = len(sorted_half1), len(sorted_half2)
 
-    _sorted = [0] * (half1_length + half2_length)
     while i != half1_length and j != half2_length:
         if sorted_half1[i] < sorted_half2[j]:
-            _sorted[k] = sorted_half1[i]
+            unsorted[k] = sorted_half1[i]
             i += 1
         else:
-            _sorted[k] = sorted_half2[j]
+            unsorted[k] = sorted_half2[j]
             j += 1
         k += 1
 
-    _sorted[k:] = sorted_half1[i:] if i != half1_length else sorted_half2[j:]
-    return _sorted
+    unsorted[k:] = sorted_half1[i:] if i != half1_length else sorted_half2[j:]
+    return unsorted
 
 
 # test case
-print(merge_sort([1, 3, 2, 5, 4]))
+list1 = [1, 3, 2, 5, 4]
+merge_sort(list1)
+print(list1)
