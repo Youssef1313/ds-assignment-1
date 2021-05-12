79887ea2733a41e77419c5c93d08d28f20e7583c
Youssef Victor <youssefvictor00@gmail.com>
2021-05-12 23:24:20 +0200

    Get unit tests working partially (#29)
    * Get unit tests working partially
    
    * Import function instead of module

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,55 +1,82 @@
-# TODO: Remove noqa in all tests.
-
-def test_none():
-    input = None  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
-
-
-def test_not_a_list():
-    input = "Hello world"  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+import pytest
+from src.sorting.bubble_sort import bubble_sort
+from src.sorting.heap_sort import heap_sort
+from src.sorting.insertion_sort import insertion_sort
+from src.sorting.merge_sort import merge_sort
+from src.sorting.selection_sort import selection_sort
 
 
-def test_list_containing_none():
-    input = [1, None, 5]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_none(sort_algorithm):
+    input = None
+    with pytest.raises(TypeError):
+        assert_sorted(input, sort_algorithm)
 
 
-def test_list_containing_string():
-    input = [1, "Hello world", 5]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_not_a_list(sort_algorithm):
+    input = "Hello world"
+    with pytest.raises(TypeError):
+        assert_sorted(input, sort_algorithm)
 
 
-def test_empty_list():
-    input = []  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_list_containing_none(sort_algorithm):
+    input = [1, None, 5]
+    with pytest.raises(TypeError):
+        assert_sorted(input, sort_algorithm)
 
 
-def test_single_element():
-    input = [5]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_list_containing_string(sort_algorithm):
+    input = [1, "Hello world", 5]
+    with pytest.raises(TypeError):
+        assert_sorted(input, sort_algorithm)
 
 
-def test_two_elements_already_sorted():
-    input = [1, 3]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_empty_list(sort_algorithm):
+    input = []
+    assert_sorted(input, sort_algorithm)
 
 
-def test_two_elements_unsorted():
-    input = [3, 1]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_single_element(sort_algorithm):
+    input = [5]
+    assert_sorted(input, sort_algorithm)
 
 
-def test_integers_and_floats():
-    input = [5, 3.1, 3, 5.5, 1, 2.1]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_two_elements_already_sorted(sort_algorithm):
+    input = [1, 3]
+    assert_sorted(input, sort_algorithm)
 
 
-def test_repeated_values():
-    input = [5, 3.1, 3, 5.5, 1, 2.1, 3.0, 1]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_two_elements_unsorted(sort_algorithm):
+    input = [3, 1]
+    assert_sorted(input, sort_algorithm)
 
 
-def test_sorting1():
-    input = [5, 3, 1, 9, 8, 4, 3, 2, 0, 9, 16, 15, 17]  # noqa: F841
-    assert True  # TODO: Test against the 6 algorithms.
+def test_integers_and_floats(sort_algorithm):
+    input = [5, 3.1, 3, 5.5, 1, 2.1]
+    assert_sorted(input, sort_algorithm)
+
+
+def test_repeated_values(sort_algorithm):
+    input = [5, 3.1, 3, 5.5, 1, 2.1, 3.0, 1]
+    assert_sorted(input, sort_algorithm)
+
+
+def test_sorting1(sort_algorithm):
+    input = [5, 3, 1, 9, 8, 4, 3, 2, 0, 9, 16, 15, 17]
+    assert_sorted(input, sort_algorithm)
+
+
+@pytest.fixture(params=[
+    bubble_sort,
+    heap_sort,
+    insertion_sort,
+    merge_sort,
+    selection_sort,
+    ])
+def sort_algorithm(request):
+    return request.param
+
+
+def assert_sorted(input, sort_algorithm):
+    expected = sorted(input)
+    sort_algorithm(input)
+    assert input == expected
