9698655deb94f91feff354874b0a73077ca930b9
Youssef1313 <youssefvictor00@gmail.com>
2021-05-13 00:30:10 +0200

    Avoid class

--- src/sorting/quick_sort.py
+++ src/sorting/quick_sort.py
@@ -1,60 +1,48 @@
 import random
-from abc import abstractmethod
 
 
-class QuickSort():
-
-    def sort(self):
-        self.__quick_sort(0, len(self.arr))
-        return self.arr
-
-    def __quick_sort(self, first, last):
-        if first >= last:
-            return
-        p: int = self.__partition(first, last - 1)
-        self.__quick_sort(first, p)
-        self.__quick_sort(p + 1, last)
-
-    def __partition(self, first, last):
-        i: int = first - 1
-        j: int = first
-        piv_ind = self._get_pivot(first, last)
-        piv = piv_ind[0]
-        ind = piv_ind[1]
-        self.arr[ind], self.arr[last] = self.arr[last], self.arr[ind]
-        while i < (last) and j < (last):
-            if self.arr[j] < piv:
-                i += 1
-                self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
-            j += 1
-        self.arr[i + 1], self.arr[last] = self.arr[last], self.arr[i + 1]
-        return i + 1
-
-    @abstractmethod
-    def _get_pivot(self, first, last):
-        pass
+def quick_sort_last_pivot(arr):
+    __quick_sort(arr, 0, len(arr), __get_pivot_last)
 
 
-class QuickSortLP(QuickSort):
-    def __init__(self, arr):
-        self.arr = arr
-
-    def _get_pivot(self, first, last):
-        return self.arr[last], last
+def quick_sort_random_pivot(arr):
+    __quick_sort(arr, 0, len(arr), __get_pivot_random)
 
 
-class QuickSortRP(QuickSort):
-    def __init__(self, arr):
-        self.arr = arr
+def __quick_sort(arr, first, last, get_pivot):
+    if first >= last:
+        return
+    p: int = __partition(arr, first, last - 1, get_pivot)
+    __quick_sort(arr, first, p, get_pivot)
+    __quick_sort(arr, p + 1, last, get_pivot)
 
-    def _get_pivot(self, first, last):
-        rand: int = random.randint(first, last)
-        return self.arr[rand], rand
+
+def __partition(arr, first, last, get_pivot):
+    i: int = first - 1
+    j: int = first
+    piv_ind = get_pivot(arr, first, last)
+    piv = piv_ind[0]
+    ind = piv_ind[1]
+    arr[ind], arr[last] = arr[last], arr[ind]
+    while i < (last) and j < (last):
+        if arr[j] < piv:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+        j += 1
+    arr[i + 1], arr[last] = arr[last], arr[i + 1]
+    return i + 1
+
+
+def __get_pivot_last(arr, first, last):
+    return arr[last], last
+
+
+def __get_pivot_random(arr, first, last):
+    rand: int = random.randint(first, last)
+    return arr[rand], rand
 
 
 # arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
 arr = [5, 3.1, 3, 5.5, 1, 2.1, 3.0, 1]
-q1 = QuickSortLP(arr)
-q2 = QuickSortRP(arr)
-print(q1.sort())
-print(q2.sort())
+q1 = quick_sort_random_pivot(arr)
+print(arr)
