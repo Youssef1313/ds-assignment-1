17d1d66562e28eb80fa2cd48267dc8278ed33d2e
mohamed zayton <mohamed-zayton>
2021-05-12 14:24:09 +0200

    ss_modify

--- src/sorting/selection_sort.py
+++ src/sorting/selection_sort.py
@@ -1,20 +1,13 @@
-import random
+def sort(arr):
+    arr_len = len(arr)
+    for i in range(arr_len):
+        min_index = i
+        for j in range(i, arr_len):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
 
 
-class SelectionSort:
-
-    @staticmethod
-    def sort(arr):
-        arr_len = len(arr)
-        for i in range(arr_len):
-            min_index = i
-            for j in range(i, arr_len):
-                if arr[min_index] > arr[j]:
-                    min_index = j
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-        return arr
-
-
-arr = [random.randrange(1, 1000000, 1) for i in range(500000)]
 arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
-print(SelectionSort.sort(arr))
+print(sort(arr))
