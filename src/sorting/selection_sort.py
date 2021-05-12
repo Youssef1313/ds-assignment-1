1fd452a9d3b2df32e91ea8e4c6b9efe76d7c684c
Youssef Victor <youssefvictor00@gmail.com>
2021-05-12 15:22:43 +0200

    Apply suggestions from code review

--- src/sorting/selection_sort.py
+++ src/sorting/selection_sort.py
@@ -1,13 +1,13 @@
 def selection_sort(unsorted):
-    arr_len = len(arr)
+    arr_len = len(unsorted)
     for i in range(arr_len):
         min_index = i
         for j in range(i, arr_len):
-            if arr[min_index] > arr[j]:
+            if unsorted[min_index] > unsorted[j]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+        unsorted[i], unsorted[min_index] = unsorted[min_index], unsorted[i]
 
 
 arr = [2, 1, -1, 8, 6, 6, 99, 30, 10, 7, 44, 99, 4]
-print(sort(arr))
+selection_sort(arr)
+print(arr)
