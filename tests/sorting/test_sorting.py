1811d54c2c37deb936755c1db3f03b154e2cd0de
MOHAND99 <muhanned122000@yahoo.com>
2021-05-15 09:36:31 +0200

    input-variable-name-edit

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -9,62 +9,62 @@ from src.sorting.quick_sort import quick_sort_random_pivot
 
 
 def test_none(sort_algorithm):
-    input = None
+    _input = None
     with pytest.raises(TypeError):
-        assert_sorted(input, sort_algorithm)
+        assert_sorted(_input, sort_algorithm)
 
 
 def test_not_a_list(sort_algorithm):
-    input = "Hello world"
+    _input = "Hello world"
     with pytest.raises(TypeError):
-        assert_sorted(input, sort_algorithm)
+        assert_sorted(_input, sort_algorithm)
 
 
 def test_list_containing_none(sort_algorithm):
-    input = [1, None, 5]
+    _input = [1, None, 5]
     with pytest.raises(TypeError):
-        assert_sorted(input, sort_algorithm)
+        assert_sorted(_input, sort_algorithm)
 
 
 def test_list_containing_string(sort_algorithm):
-    input = [1, "Hello world", 5]
+    _input = [1, "Hello world", 5]
     with pytest.raises(TypeError):
-        assert_sorted(input, sort_algorithm)
+        assert_sorted(_input, sort_algorithm)
 
 
 def test_empty_list(sort_algorithm):
-    input = []
-    assert_sorted(input, sort_algorithm)
+    _input = []
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_single_element(sort_algorithm):
-    input = [5]
-    assert_sorted(input, sort_algorithm)
+    _input = [5]
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_two_elements_already_sorted(sort_algorithm):
-    input = [1, 3]
-    assert_sorted(input, sort_algorithm)
+    _input = [1, 3]
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_two_elements_unsorted(sort_algorithm):
-    input = [3, 1]
-    assert_sorted(input, sort_algorithm)
+    _input = [3, 1]
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_integers_and_floats(sort_algorithm):
-    input = [5, 3.1, 3, 5.5, 1, 2.1]
-    assert_sorted(input, sort_algorithm)
+    _input = [5, 3.1, 3, 5.5, 1, 2.1]
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_repeated_values(sort_algorithm):
-    input = [5, 3.1, 3, 5.5, 1, 2.1, 3.0, 1]
-    assert_sorted(input, sort_algorithm)
+    _input = [5, 3.1, 3, 5.5, 1, 2.1, 3.0, 1]
+    assert_sorted(_input, sort_algorithm)
 
 
 def test_sorting1(sort_algorithm):
-    input = [5, 3, 1, 9, 8, 4, 3, 2, 0, 9, 16, 15, 17]
-    assert_sorted(input, sort_algorithm)
+    _input = [5, 3, 1, 9, 8, 4, 3, 2, 0, 9, 16, 15, 17]
+    assert_sorted(_input, sort_algorithm)
 
 
 @pytest.fixture(params=[
@@ -80,7 +80,7 @@ def sort_algorithm(request):
     return request.param
 
 
-def assert_sorted(input, sort_algorithm):
-    expected = sorted(input)
-    sort_algorithm(input)
-    assert input == expected
+def assert_sorted(_input, sort_algorithm):
+    expected = sorted(_input)
+    sort_algorithm(_input)
+    assert _input == expected
