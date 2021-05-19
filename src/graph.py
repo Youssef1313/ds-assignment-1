bec9933d951848ec849368d5dfca5af2490f0d29
Youssef Victor <youssefvictor00@gmail.com>
2021-05-19 20:37:33 +0200

    More neater graph (#56)
    * graph_pt
    
    * Apply suggestions from code review
    
    * Update graph.py
    
    * Update src/benchmarks.py
    
    * Update src/benchmarks.py
    
    * Update report
    
    * Lower to 1500
    
    Co-authored-by: mohamed-zayton <mohamed-zayton>

--- src/graph.py
+++ src/graph.py
@@ -3,11 +3,11 @@ from matplotlib import pyplot as plt
 
 def graph(algorithms_runtimes):
     x_axis = algorithms_runtimes.x_axis
-    plt.yscale("log")
-    plt.xscale("log")
+    # plt.yscale("log")
+    # plt.xscale("log")
     for display_name, run_times in algorithms_runtimes.algorithms.items():
         plt.plot(x_axis, run_times, label=display_name)
 
-    plt.title("Logarithmic scale of sorting algorithms run times")
+    plt.title("Sorting algorithms run times")
     plt.legend()
     plt.show()
